e1aa2409ad6d946347548e6d9eefb114f68d550d
Henning Ottesen <henning@live.no>
2018-03-29 01:53:19 +0200

    Problem 6

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 mod p1;
 mod p2;
+mod p6;
 mod p15;
 mod p22;
 mod p31;
 
 pub use p1::p1;
 pub use p2::p2;
+pub use p6::p6;
 pub use p15::p15;
 pub use p22::p22;
 pub use p31::p31;
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use project_euler as pe;
 fn main() {
     println!("Problem 1: {}", pe::p1());
     println!("Problem 2: {}", pe::p2());
+    println!("Problem 6: {}", pe::p6());
     println!("Problem 15: {}", pe::p15());
     println!("Problem 22: {}", pe::p22());
     println!("Problem 31: {}", pe::p31());
