0295c61be0b40fa7edf9690df6e48c64a7b072b5
Henning Ottesen <henning@live.no>
2018-03-29 00:11:23 +0200

    Problem 22

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 mod p1;
 mod p2;
 mod p15;
+mod p22;
 
 pub use p1::p1;
 pub use p2::p2;
 pub use p15::p15;
+pub use p22::p22;
--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,5 @@ fn main() {
     println!("Problem 1: {}", pe::p1());
     println!("Problem 2: {}", pe::p2());
     println!("Problem 15: {}", pe::p15());
+    println!("Problem 22: {}", pe::p22());
 }
