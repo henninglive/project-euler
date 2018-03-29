093987c57094ecfd921fafa678cde88a23d07629
Henning Ottesen <henning@live.no>
2018-03-29 17:00:29 +0200

    Problem 8

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ fn problems() -> BTreeMap<usize, Problem> {
     problems.insert(4, pe::p4 as Problem);
     problems.insert(5, pe::p5 as Problem);
     problems.insert(6, pe::p6 as Problem);
+    problems.insert(8, pe::p8 as Problem);
     problems.insert(15, pe::p15 as Problem);
     problems.insert(22, pe::p22 as Problem);
     problems.insert(31, pe::p31 as Problem);
